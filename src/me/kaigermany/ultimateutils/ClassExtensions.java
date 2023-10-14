8c72f00565336bab7f869348083c5eff276fa6eb
kaigermany <48881973+kaigermany@users.noreply.github.com>
2023-10-14 23:15:19 +0200

    added code comments.
    maybe the code here will be move into an external class, but that should not be a big problem.

--- src/me/kaigermany/ultimateutils/ClassExtensions.java
+++ src/me/kaigermany/ultimateutils/ClassExtensions.java
@@ -12,6 +12,12 @@ import me.kaigermany.ultimateutils.classextensioninternals.TIME;
 public interface ClassExtensions extends
 IO, MATH, FastHex, TIME, SYSOUT, IMAGE
 {
+	/**
+	 * Merges a given String[] and add a given String in between.
+	 * @param in Arguments to merge
+	 * @param insert The word that will be added in between each argument
+	 * @return the combined data
+	 */
 	default String join(String[] in, String insert) {
 		if (in == null || in.length == 0)
 			return "";
@@ -21,14 +27,26 @@ IO, MATH, FastHex, TIME, SYSOUT, IMAGE
 		return sb.toString().substring(insert.length());
 	}
 	
+	/**
+	 * A simplified version of String.split() - it has NO REGEX! If you want to use REGEX, consider using String.split() instead!
+	 * This split function just searches for the exact occurrence of the phrase String filter and nothing else.
+ 	 * @param in argument to split
+	 * @param filter word to search for
+	 * @return a list of all arguments before and after each hit of the word search.
+	 */
 	default String[] split(String in, String filter) {
 		ArrayList<String> items = splitToArray(in, filter);
-		String[] outgoing = new String[items.size()];
-		items.toArray(outgoing);
-		return outgoing;
+		return items.toArray(new String[items.size()]);
 	}
 	
-	static ArrayList<String> splitToArray(String in, String filter) {
+	/**
+	 * A simplified version of String.split() - it has NO REGEX! If you want to use REGEX, consider using String.split() instead!
+	 * This split function just searches for the exact occurrence of the phrase String filter and nothing else.
+ 	 * @param in argument to split
+	 * @param filter word to search for
+	 * @return a list of all arguments before and after each hit of the word search.
+	 */
+	default ArrayList<String> splitToArray(String in, String filter) {
 		int index, offset;
 		ArrayList<String> items = new ArrayList<String>();
 		if (in == null || in.length() == 0 || filter == null) {
