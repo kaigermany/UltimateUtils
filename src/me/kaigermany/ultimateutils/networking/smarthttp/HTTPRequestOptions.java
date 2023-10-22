54fe8b1e39cbc2dd67674ebd2f0a867edb19d620
kaigermany <48881973+kaigermany@users.noreply.github.com>
2023-10-23 00:13:06 +0200

    added HTTPRequestOptions to optimize SmartHTTP

---
+++ src/me/kaigermany/ultimateutils/networking/smarthttp/HTTPRequestOptions.java
@@ -0,0 +1,162 @@
+package me.kaigermany.ultimateutils.networking.smarthttp;
+
+import java.util.HashMap;
+
+public class HTTPRequestOptions {
+	private final String server;
+	private final int port;
+	private final String page;
+	
+	private String requestMethod = "GET";
+	private HashMap<String, String> headerFields;
+	private byte[] postData;
+	private int maxSocketCount;
+	private boolean useSSL;
+	private boolean disableCertificateCheck;
+	private HTTPResultEvent event;
+	
+	public HTTPRequestOptions(String url){
+		String[] urlElements = parseUrl(url);
+		int port;
+		boolean ssl = urlElements[0] != null && urlElements[0].equals("https");
+		if(urlElements[2] == null) {
+			port = ssl ? 443 : 80;
+		} else {
+			port = Integer.parseInt(urlElements[2]);
+		}
+		this.server = urlElements[0];
+		this.port = port;
+		this.page = urlElements[3];
+	}
+	
+	public HTTPRequestOptions(String server, int port, String page){
+		this.server = server;
+		this.port = port;
+		this.page = page;
+	}
+	
+	public String getServer(){
+		return server;
+	}
+	
+	public int getPort(){
+		return port;
+	}
+	
+	public String getPage(){
+		return page;
+	}
+	
+	public HTTPRequestOptions setRequestMethod(String requestMethod){
+		this.requestMethod = requestMethod;
+		return this;
+	}
+	
+	public String getRequestMethod(){
+		return requestMethod;
+	}
+	
+	public HTTPRequestOptions setRequestMethod(HashMap<String, String> headerFields){
+		this.headerFields = headerFields;
+		return this;
+	}
+	
+	public HashMap<String, String> getHeaderFields(){
+		return headerFields;
+	}
+	
+	public HTTPRequestOptions setPostData(byte[] postData){
+		this.postData = postData;
+		return this;
+	}
+	
+	public byte[] getPostData(){
+		return postData;
+	}
+	
+	public HTTPRequestOptions setMaxSocketCount(int maxSocketCount){
+		this.maxSocketCount = maxSocketCount;
+		return this;
+	}
+	
+	public int getMaxSocketCount(){
+		return maxSocketCount;
+	}
+	
+	public HTTPRequestOptions setUseSSL(boolean useSSL){
+		this.useSSL = useSSL;
+		return this;
+	}
+	
+	public boolean getUseSSL(){
+		return useSSL;
+	}
+	
+	public HTTPRequestOptions setDisableCertificateCheck(boolean disableCertificateCheck){
+		this.disableCertificateCheck = disableCertificateCheck;
+		return this;
+	}
+	
+	public boolean getDisableCertificateCheck(){
+		return disableCertificateCheck;
+	}
+	
+	public HTTPRequestOptions setEvent(HTTPResultEvent event){
+		this.event = event;
+		return this;
+	}
+	
+	public HTTPResultEvent getEvent(){
+		return event;
+	}
+	
+	public HTTPRequestOptions dublicate(){
+		HTTPRequestOptions copy = new HTTPRequestOptions(server, port, page);
+		copy.requestMethod = this.requestMethod;
+		copy.headerFields = this.headerFields;
+		copy.postData = this.postData;
+		copy.maxSocketCount = this.maxSocketCount;
+		copy.useSSL = this.useSSL;
+		copy.disableCertificateCheck = this.disableCertificateCheck;
+		copy.event = this.event;
+		return copy;
+	}
+	
+	private static String[] parseUrl(String url){
+		String protocol = null;
+		int p = url.indexOf("://");
+		if(p != -1) {
+			protocol = url.substring(0, p);
+			url = url.substring(p + 3);
+		}
+		p = url.indexOf('/');
+		String serverName;
+		String targetPath;
+		if(p == -1){
+			serverName = url;
+			targetPath = "/";
+		} else {
+			serverName = url.substring(0, p);
+			targetPath = url.substring(p);
+		}
+		
+		String ip;
+		String port = null;
+		p = serverName.lastIndexOf(':');
+		int p2 = serverName.lastIndexOf(']');
+		if(p2 > p) {//mismatch-test:
+			//pattern: "[ip:0::v6]:port"
+			//so, if ':' is last found
+			//before ']' then it's part of IPv6 and not the port
+			p = -1;
+		}
+		if(p == -1){
+			ip = serverName;
+		} else {
+			ip = serverName.substring(0, p);
+			port = serverName.substring(p + 1);
+		}
+		
+		return new String[]{protocol, ip, port, targetPath};
+	}
+}
