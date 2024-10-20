64422240e1c915046b0e291b84e9c62baafcfad1
kaigermany <48881973+kaigermany@users.noreply.github.com>
2024-10-21 00:12:44 +0200

    Added static utilities to provide speed optimized methods

---
+++ src/me/kaigermany/ultimateutils/StaticUtils.java
@@ -0,0 +1,271 @@
+package me.kaigermany.ultimateutils;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import javax.imageio.ImageIO;
+
+import me.kaigermany.ultimateutils.classextensioninternals.FastHex;
+
+public class StaticUtils {
+	private StaticUtils(){}
+	
+	public static byte[] readAllBytes(InputStream is) throws IOException {
+		byte[] buf = new byte[4096];
+		int l;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream(4096);
+		while((l = is.read(buf)) != -1){
+			baos.write(buf, 0, l);
+		}
+		return baos.toByteArray();
+	}
+	
+	public static BufferedImage loadImage(File file) throws IOException {
+		return ImageIO.read(file);
+	}
+	
+	public static BufferedImage loadImage(InputStream is) throws IOException {
+		return ImageIO.read(is);
+	}
+
+	public static void saveImage(BufferedImage image, File file) throws IOException {
+		String name = file.getName();
+		int pointPos = name.lastIndexOf('.');
+		if(pointPos == -1) throw new IOException("unable to identify file type from given filename");
+		saveImage(image, name.substring(pointPos + 1), file);
+	}
+
+	public static void saveImage(BufferedImage image, String type, File file) throws IOException {
+		ImageIO.write(image, type, file);
+	}
+	
+	public static void saveImage(BufferedImage image, String type, OutputStream os) throws IOException {
+		ImageIO.write(image, type, os);
+	}
+	
+	public static void hexDump(byte[] in) {
+		int offset = 0;
+		FastHex h = new FastHex(){};
+		while (true) {
+			System.out.print(h.hexInt(offset) + " ");
+			for (int i = 0; i < 16; i++) {
+				int a = offset + i;
+				if (a < in.length)
+					System.out.print(h.hexByte(in[a]) + " ");
+				else
+					System.out.print("   ");
+			}
+			for (int i = 0; i < 16; i++) {
+				int a = offset + i;
+				if (a < in.length)
+					System.out.print((char) in[a]);
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+			offset += 16;
+			if (offset >= in.length)
+				break;
+		}
+	}
+	
+	public static Iterable<Map.Entry<ZipEntry, byte[]>> zipIterator(File file) throws IOException {
+		final ZipFile zf = new ZipFile(file);
+		final Enumeration<? extends ZipEntry> iterator = zf.entries();
+		return new Iterable<Map.Entry<ZipEntry, byte[]>>(){
+			@Override
+			public Iterator<Map.Entry<ZipEntry, byte[]>> iterator() {
+				return new Iterator<Map.Entry<ZipEntry, byte[]>>() {
+					@Override
+					public boolean hasNext() {
+						boolean next = iterator.hasMoreElements();
+						if(!next){
+							try {
+								zf.close();
+							} catch (IOException e) {
+								RuntimeException ex = new RuntimeException("unable to close ZipFile");
+								ex.addSuppressed(e);
+								throw ex;
+							}
+						}
+						return next;
+					}
+
+					@Override
+					public Entry<ZipEntry, byte[]> next() {
+						final ZipEntry entry = iterator.nextElement();
+						
+						return new Map.Entry<ZipEntry, byte[]>(){
+
+							@Override
+							public ZipEntry getKey() {
+								return entry;
+							}
+
+							@Override
+							public byte[] getValue() {
+								try {
+									return readAllBytes(zf.getInputStream(entry));
+								} catch (IOException e) {
+									RuntimeException ex = new RuntimeException("unable to read ZipFile entry");
+									ex.addSuppressed(e);
+									throw ex;
+								}
+							}
+
+							@Override
+							public byte[] setValue(byte[] value) {
+								return null;
+							}
+							
+						};
+					}
+					
+				};
+			}
+		};
+	}
+	
+	public static Iterable<Map.Entry<ZipEntry, byte[]>> zipIterator(InputStream is) throws IOException {
+		final ZipInputStream zis = new ZipInputStream(new BufferedInputStream(is, 1 << 20));
+		return new Iterable<Map.Entry<ZipEntry, byte[]>>(){
+			@Override
+			public Iterator<Map.Entry<ZipEntry, byte[]>> iterator() {
+				return new Iterator<Map.Entry<ZipEntry, byte[]>>() {
+					ZipEntry entry = getNextEntry();
+					
+					private ZipEntry getNextEntry(){
+						try{
+							return zis.getNextEntry();
+						} catch (IOException e) {
+							RuntimeException ex = new RuntimeException("unable to read ZipFile entry");
+							ex.addSuppressed(e);
+							throw ex;
+						}
+					}
+					
+					@Override
+					public boolean hasNext() {
+						if(entry == null) entry = getNextEntry();
+						boolean next = entry != null;
+						if(!next){
+							try {
+								zis.close();
+							} catch (IOException e) {
+								RuntimeException ex = new RuntimeException("unable to close ZipFile");
+								ex.addSuppressed(e);
+								throw ex;
+							}
+						}
+						return next;
+					}
+
+					@Override
+					public Entry<ZipEntry, byte[]> next() {
+						if(entry == null) entry = getNextEntry();
+						final ZipEntry entry = this.entry;
+						this.entry = null;
+						
+						final byte[] fileBytes;
+						
+						try {
+							fileBytes = readAllBytes(zis);
+						} catch (IOException e) {
+							RuntimeException ex = new RuntimeException("unable to read ZipFile entry");
+							ex.addSuppressed(e);
+							throw ex;
+						}
+						
+						return new Map.Entry<ZipEntry, byte[]>(){
+
+							@Override
+							public ZipEntry getKey() {
+								return entry;
+							}
+
+							@Override
+							public byte[] getValue() {
+								return fileBytes;
+							}
+
+							@Override
+							public byte[] setValue(byte[] value) {
+								return null;
+							}
+							
+						};
+					}
+					
+				};
+			}
+		};
+	}
+	
+	/**
+	 * Loads a File from disk.
+	 * the size is limited to Java's array size limit of 2 GB.
+	 * If missing, FileInputStream throws FileNotFoundException.
+	 * If there are some other Exceptions, the method will throw them.
+	 * if finally all bytes are read successfully the array will be returned.
+	 * @param file File to load
+	 * @return byte[] file contents
+	 * @throws IOException
+	 */
+	public static byte[] loadBytes(File file) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data;
+		
+		{
+			long l = file.length();
+			if((l & ~0x7FFFFFFFL) != 0) {//check if more than 31 bits in use.
+				fis.close();
+				throw new IOException("The file is too big! loadBytes() can only handle files up to 2GB");
+			}
+			data = new byte[(int)l];
+		}//drop long l.
+		
+		int off = copyStreamToArray(fis, data);
+		fis.close();
+		if(off != data.length) throw new IOException("File read was incomplete, the stream was closed too early");
+		return data;
+	}
+	
+	/**
+	 * Store a new File on disk.
+	 * If directory is missing, it will be created.
+	 * If file already exists it gets overwritten.
+	 * @param file File to save
+	 * @param data raw bytes to save
+	 * @throws IOException
+	 */
+	public static void saveBytes(File file, byte[] data) throws IOException {
+		File dir = file.getParentFile();
+		if(!dir.exists()) dir.mkdirs();
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(data);
+		fos.close();
+	}
+	
+	public static int copyStreamToArray(InputStream is, byte[] out) throws IOException {
+		int off = 0;
+		int len;
+		while(out.length - off > 0 && (len = is.read(out, off, out.length - off)) != -1){
+			off += len;
+		}
+		return off;
+	}
+}
