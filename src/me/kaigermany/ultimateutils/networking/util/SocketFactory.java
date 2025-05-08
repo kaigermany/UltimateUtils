8b48a9b5cd86b24a324e2a966a42e30765a182af
TheHolyException <patrick.knoblich@proton.me>
2025-05-08 20:23:18 +0200

    Adding Proxy Support

--- src/me/kaigermany/ultimateutils/networking/util/SocketFactory.java
+++ src/me/kaigermany/ultimateutils/networking/util/SocketFactory.java
@@ -1,6 +1,8 @@
 package me.kaigermany.ultimateutils.networking.util;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.SecureRandom;
@@ -15,16 +17,62 @@ import javax.net.ssl.X509TrustManager;
 public class SocketFactory {
 	private static final javax.net.SocketFactory secureSSLFactory = SSLSocketFactory.getDefault();
 	private static final javax.net.SocketFactory insecureSSLFactory = getSocketFactory();
-	
-	public static Socket openConnection(String ip, int port, boolean ssl, boolean disableCertificateCheck) throws UnknownHostException, IOException {
-		if(ssl){
-			return (disableCertificateCheck ? insecureSSLFactory : secureSSLFactory).createSocket(ip, port);
+	private static Proxy proxy;
+
+	/**
+	 * Creates a new Socket. <br />
+	 * if ssl then an new SSLSocket will be opened. <br />
+	 * if ssl && disableCertificateCheck then an new SSLSocket will be opened
+	 * but the certificate will not be checked. This can become handy if you are
+	 * working with self-signed or out-dated certificates.
+	 * 
+	 * @param ip IPv4, PIv6 or DNS address to connect to
+	 * @param port the target port to open
+	 * @param ssl use an SSLSocket or else a plain (raw) Socket
+	 * @param disableCertificateCheck ignores invalid or unknown certificates. works only if boolean ssl is also true.
+	 * @return a new Socket ready to communicate.
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public static Socket openConnection(String ip, int port, boolean ssl, boolean disableCertificateCheck)
+		throws UnknownHostException, IOException {
+		Socket socket;
+
+		if (proxy != null) {
+			if (ssl) {
+				socket = new Socket(proxy);
+				socket.connect(new InetSocketAddress(ip, port));
+
+				javax.net.SocketFactory factory = disableCertificateCheck ? insecureSSLFactory : secureSSLFactory;
+
+				if (factory instanceof javax.net.ssl.SSLSocketFactory) {
+					return ((javax.net.ssl.SSLSocketFactory) factory).createSocket(
+						socket, ip, port, true /* autoClose */);
+				} else {
+					throw new IOException("SSL über SOCKS-Proxy benötigt eine SSLSocketFactory");
+				}
+			} else {
+				socket = new Socket(proxy);
+				socket.connect(new InetSocketAddress(ip, port));
+				return socket;
+			}
 		} else {
-			return new Socket(ip, port);
+			if (ssl) {
+				return (disableCertificateCheck ? insecureSSLFactory : secureSSLFactory).createSocket(ip, port);
+			} else {
+				return new Socket(ip, port);
+			}
 		}
 	}
-	
-	//based on https://stackoverflow.com/questions/12060250/ignore-ssl-certificate-errors-with-java
+
+	/**
+	 * Creates a new SSLSocketFactory that ignores all certificate validations
+	 * based on: <a href=
+	 * "https://stackoverflow.com/questions/12060250/ignore-ssl-certificate-errors-with-java">
+	 * ...</a>
+	 *
+	 * @return SSLSocketFactory
+	 */
 	private static SSLSocketFactory getSocketFactory() {
 		TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
 			@Override public X509Certificate[] getAcceptedIssuers() { return null; }
@@ -34,12 +82,17 @@ public class SocketFactory {
 	    
 	    SSLContext ctx = null;
 	    try {
-	        ctx = SSLContext.getInstance("SSL");
+	    	SSLContext ctx = SSLContext.getInstance("SSL");
 	        ctx.init(null, certs, new SecureRandom());
+		    return ctx.getSocketFactory();
 	    } catch (java.security.GeneralSecurityException ex) {
 	    	ex.printStackTrace();
 	    	return null;
 	    }
 	    return ctx.getSocketFactory();
+	}
+
+	public static void setProxy(Proxy proxy) {
+		SocketFactory.proxy = proxy;
 	}
 }
