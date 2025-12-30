bfba29bfa090c6f67858069141cc3b71fef117d2
kaigermany <48881973+kaigermany@users.noreply.github.com>
2025-12-30 12:38:53 +0100

    Revert "WebSocketEvent is now fully abstract"
    This reverts commit 8707aaab249fd471edd2544a009867a7513c8c33.

--- src/me/kaigermany/ultimateutils/networking/websocket/WebSocketEvent.java
+++ src/me/kaigermany/ultimateutils/networking/websocket/WebSocketEvent.java
@@ -2,14 +2,14 @@ package me.kaigermany.ultimateutils.networking.websocket;
 
 public abstract class WebSocketEvent {
 
-    public abstract void onOpen(WebSocketBasic socket);
+    public void onOpen(WebSocketBasic socket) {}
 
-    public abstract void onBinary(byte[] data, WebSocketBasic socket);
+    public void onBinary(byte[] data, WebSocketBasic socket) {}
 
-    public abstract void onMessage(String data, WebSocketBasic socket);
+    public void onMessage(String data, WebSocketBasic socket) {}
 
-    public abstract void onClose(WebSocketBasic socket);
+    public void onClose(WebSocketBasic socket) {}
 
-    public abstract void onError(WebSocketBasic socket, String message, Exception exception);
+    public void onError(WebSocketBasic socket, String message, Exception exception) {}
 
 }
